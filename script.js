8cc743e690e2141e6ddea2bc899ca3e806db5257
fenomenogr777 <nc93gate7@hotmail.com>
2023-01-14 21:06:38 +0200

    input API

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 "use strict";
+const btnEL = document.querySelector("#btn");
+const jokeEL=document.querySelector("#joke")
+const apiKey = "Qwl5+75I9DXtOQFKazelyQ==UcQ3BFwo90tS46lP";
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+};
 
-const btnEL=document.querySelector("#btn")
+async function joke() {
+  const response = await fetch(apiURL, options);
+  const data = await response.json();
+  const joke=data[0].joke
+ jokeEL.innerHTML=`"${joke}"`
+}
 
-btnEL.addEventListener("click",function(){
-
-console.log(`nai`)
-
-})
+btnEL.addEventListener("click", joke);
