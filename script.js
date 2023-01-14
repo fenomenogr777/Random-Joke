34add23bbe006a060c8c8b0e1f03783546363c4e
fenomenogr777 <nc93gate7@hotmail.com>
2023-01-14 22:13:42 +0200

    update api error situtation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 const btnEL = document.querySelector("#btn");
-const jokeEL=document.querySelector("#joke")
+const jokeEL = document.querySelector("#joke");
 const apiKey = "Qwl5+75I9DXtOQFKazelyQ==UcQ3BFwo90tS46lP";
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
 const options = {
@@ -11,10 +11,20 @@ const options = {
 };
 
 async function joke() {
-  const response = await fetch(apiURL, options);
-  const data = await response.json();
-  const joke=data[0].joke
- jokeEL.innerHTML=`"${joke}"`
+  try {
+    jokeEL.innerText = "Updating...";
+    btnEL.innerText = "loading...";
+    btnEL.disabled = true;
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+    const joke = data[0].joke;
+    btnEL.innerText = "TELL ME A JOKE";
+    jokeEL.innerHTML = `"${joke}"`;
+    btnEL.disabled = false;
+  } catch (error) {
+    jokeEL.innerHTML = `An error happened,try again 😕`;
+    btnEL.innerText = "TELL ME A JOKE";
+  }
 }
 
 btnEL.addEventListener("click", joke);
